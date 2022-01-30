d2796ce9466deacb95433c8687515376c75fe2f2
Grieffin-Twilight <97674340+Grieffin-Twilight@users.noreply.github.com>
2022-01-31 07:57:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html> 
+<html lang="en">
+    <head>
+        <title>Project</title>
+        <link rel="stylesheet" href="/css/real.css">
+    </head>
+
+    <body text="blue">
+        <ul>
+            <li> <a href="#";target="blank"> <img class="inazauma";img src="/images/plus.jpg"> </a> </li>
+            <li> <a href="#";target="blank"> <img class="inazauma";img src="/images/newspaper.jpg"> </a></li>
+            <li> <a href="#";target="blank"> <img class="inazauma";img src="/images/bible.jpg"> </a> </li>
+        </ul>
+    <h1 align="center"d> <br> Hi Teachers! </h1>
+    
+    <p align="center"> Please read this! <br>
+    There are three images above, and one of them you should click</p>
+<center>
+    <table border="15px"> <caption> Guide </caption>
+        <tr> 
+            <td>For Cae content <br> please click </td>
+            <td>For Cle content<br>please click</td>
+            <td>For Math content<br> pleaase click    </td>
+        </tr>
+        <tr>
+            <td>The news paper </td>
+            <td>The bible </td>
+            <td>The plus sign </td>
+        </tr>
+    </table>
+</center>
+    <p> And for the credits, please click the link below for who contributed in out project.
+        Thank you teachers for following the intructions, have a wonderful day/afternoon/night.
+    </p>
+    </img>
+    </div>  
+ 
+    </body>
+
+</html>
