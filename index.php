d5fbf5e16a49418d1702e7726689a6604cae083a
Grieffin-Twilight <97674340+Grieffin-Twilight@users.noreply.github.com>
2022-02-14 18:43:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,54 +1,29 @@
-<!DOCTYPE html> 
-<html lang="en">
+<html>
     <head>
-        <title>Project</title>
-        <link rel="stylesheet" href="/css/real.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/Start.css">
+        <title>Press Start</title>
+
     </head>
-
-    <body text="blue">
-    <header>    
-        <ul>
-            <li> <a href="/actual-projects/Math.html"; target="blank"> <img class="inazauma";img src="/images/plus.jpg"> </a> </li>
-            <li> <a href="/actual-projects/cae.html"; target="blank"> <img class="inazauma";img src="/images/newspaper.jpg"> </a></li>
-            <li> <a href="/actual-projects/cle.html"; target="blank"> <img class="inazauma";img src="/images/bible.jpg"> </a> </li>
-        </ul>
-    </header>
-
-<content>
-    
-    <p class="jojo"; align="center"> <br> Hi Teachers! </h1>
-    
-    <center>
-    <table border="15px"> <caption> Instructions </caption>
-        <tr> 
-            <td colspan="3"> Cae </td>
-            <td> Cle </td>
-            <td> Math</td>
-        </tr>
-        <tr>
-            <th colspan="2"></th>
-            <td> Click the news paper </td>
-            <td> Clck the bible </td>
-            <td> Click the plus </td>
-        </tr>
-    </table>
-</center>
-<p align="center"> And for the extras, <br>They will be coming soon,<br> with the link below,<br> you will see who contributed in our project.
-    <br>Thank you teachers for following the intructions.<br> have a wonderful day/afternoon/night.
-</p>
-</content>
-<footer>
-    <p align="center">Group
-       <br> Alexandra fulledo
-       <br> Jayrael Nino
-       <br> Mj Dechosa
-       <br> Steven John D. Abetong
-       <br> Tristan Bryan
-    </p>
-    <p>Extras</p>
-<Marquee>
-    <p>Coming soon</p>
-</Marquee>
-</footer>  
-</body>
+    <body>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <center>
+        <a href="/actual-projects/homepage.html"><p>Start!</p></a>
+        </center>
+    </body>
 </html>
